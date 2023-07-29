2d8b2837e75304a10cb1d1d99972022f96cbdba9
Chrissem98 <chrissem19@gmail.com>
2023-07-29 14:15:29 +0000

    feat: Integrate dashcode to laravel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/loader.css', 'resources/js/main.js'],
+      refresh: true,
+    }),
+    vue({
+      template: {
+          transformAssetUrls: {
+              base: null,
+              includeAbsolute: false,
+          },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue", ".css"],
+    alias: {
+      '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+      '@css': fileURLToPath(new URL('./resources/css', import.meta.url)),
+    },
+  },
 });
